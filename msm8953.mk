847f42c00cf4035bd8e54557986051c1d0e699f7
DevStark <devstark15@gmail.com>
2018-01-16 18:34:20 +0000

    msm8953-common: Add PixelStyle

--- msm8953.mk
+++ msm8953.mk
@@ -365,3 +365,11 @@ PRODUCT_COPY_FILES += \
 # Offline charging animation
 PRODUCT_PACKAGES += \
     charger_res_images
+
+# Google Lens
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017.xml:system/etc/sysconfig/pixel_2017.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml
+#    $(LOCAL_PATH)/pixelstyle/vendor/overlay/RoundMaskOverlay.apk:vendor/overlay/RoundMaskOverlay.apk
